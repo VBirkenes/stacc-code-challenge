8ee61c4b0b4f65554cc380368939d7d713c343ef
Vegard Birkenes <vegard.birkenes@gmail.com>
2022-10-02 10:20:30 +0200

    finishing touches

--- src/main/java/com/example/demo/LoadDatabase.java
+++ src/main/java/com/example/demo/LoadDatabase.java
@@ -14,7 +14,8 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(CustomerRepository repository) {
         return args -> {
-            log.info("Preloading " + repository.save(new Customer("NK-23wLbkLretoJxTci3VMZwE","Person", "Joe Biden", "Baiden Džo", "1942-11-20", "us", "", "Q6279", "", "", "", "Every Politician", "2021-07-26 11:55:45", "2021-10-07 03:02:59")));
+            log.info("Preloading " + repository.save(new Customer("evpo-ff10bc5a-4aad-4895-b886-fb88e598ed57","Person", "Knut Arild Hareide", "Hareide, Knut Arild", "1972-11-23", "no", "", "Q1350399", "", "", "", "Every Politician", "2021-07-26 11:55:45", "2021-10-07 03:02:59")));
+            log.info("Preloading " + repository.save(new Customer("NK-23wLbkLretoJxTci3VMZwE","Person", "Joe Biden", "Baiden Džo;Biden, Joseph", "1942-11-20", "us", "", "Q6279", "", "", "", "Every Politician", "2021-07-26 11:55:45", "2021-10-07 03:02:59")));
         };
     }
     
